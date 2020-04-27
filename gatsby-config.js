982d30755a81e3e2e7d5aa64948870ad5a2f72df
zebraengine <zebraengine@users.noreply.github.com>
2020-04-27 15:27:05 -0400

    tracking id change

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-30027142-1",
+        trackingId: "G-95Q05ZCLC1",
         head: true,
       }
     },
